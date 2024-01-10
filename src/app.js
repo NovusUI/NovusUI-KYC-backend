4a4e37c019829cc91ddae31ceb1b7a594ef70192
NovusUI <noviceui@gmail.com>
2024-01-11 00:00:26 +0100

    implement twillio and nodemailer auth

---
+++ src/app.js
@@ -0,0 +1,173 @@
+const express = require("express")
+const authMiddleware = require("./middleware/authMiddleWare")
+const cors = require("cors")
+const admin = require("../firebase-config")
+const { generateFourDigitCode, sendVerificationEmail } = require("./utils")
+
+
+
+
+require("dotenv").config()
+const port = process.env.PORT ||5000
+
+
+//twillio
+const accountSid = "ACbe7638a0e216d47d25f1d27463dfdc90";
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const verifySid = "VAb71998345bdfd771e391e7c313f9649c";
+const client = require("twilio")(accountSid, authToken);
+
+const app = express()
+
+const allowedOrigins = ["http://localhost:3000"];
+
+const corsOptions =  {
+    origin:allowedOrigins,
+
+}
+
+app.use(cors(corsOptions))
+
+app.use(authMiddleware)
+app.use(express.json())
+
+
+
+// send verification code to user, and store code in the backend
+app.post("/ap1/v1/email-verification/send-email",async(req,res)=>{
+    
+     const code = generateFourDigitCode()
+    //save code to database with the user as the id
+    const data = {
+        emailVerificationCode:code ,
+        timeStamp: new Date()
+    }
+
+    const db = admin.firestore()
+
+    try {
+        const verificationCodeRef = db.collection("verificationCodes").doc(req.user.uid)
+        await verificationCodeRef.set(data)
+        
+        sendVerificationEmail(req.body.email,code,(error)=>{
+                if(error){
+                    res.status(500).json({msg:"an error occured"})
+                }else{
+                    res.status(200).json({msg:"sent"})
+                }
+        })
+        
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({msg: "server error"})
+    }
+    
+})
+
+
+app.post("/ap1/v1/email-verification/validate-code",async(req,res)=>{
+    
+     
+    //save code to database with the user as the id
+    console.log(req.body)
+
+    const db = admin.firestore()
+
+    try {
+        const verificationCodeRef = db.collection("verificationCodes").doc(req.user.uid)
+        const verificationCode = await verificationCodeRef.get()
+    
+        if(verificationCode.data().emailVerificationCode === Number(req.body.token)){
+            const userRef = db.collection("users").doc(req.user.uid)
+            const userdata = (await userRef.get()).data()
+
+           
+            const verification = {
+                ...userdata.verification,
+                email: "completed"
+            }
+            await userRef.update({verification})
+            res.status(200).json({mgs: "accepted"})
+        }else{
+            res.status(401).json({mgs: "unauthorized"})
+        }
+        
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({msg: "server erroe"})
+    }
+
+
+    
+})
+
+
+
+// send verification to number
+app.post("/ap1/v1/phone-verification/send-otp", (req,res)=>{
+
+    client.verify.v2
+    .services(verifySid)
+    .verifications.create({ to: req.body.phone, channel: "sms" })
+    .then((verification) => {
+        console.log(verification.status)
+        res.status(200).json({msg:verification.status })
+    })
+    .catch(e=>{
+        console.log(e)
+        res.status(500).json({msg: "server error"})
+    })
+    
+})
+
+
+
+// verify otp
+
+app.post("/ap1/v1/phone-verification/validate-otp",async(req,res)=>{
+    
+     
+    //save code to database with the user as the id
+    console.log(req.body)
+
+    const db = admin.firestore()
+
+    client.verify.v2
+    .services(verifySid)
+    .verificationChecks.create({ to: req.body.phone , code: Number(req.body.token) })
+    .then((verification_check) => console.log(verification_check.status))
+    .then(async() =>{
+
+        try {
+ 
+            const userRef = db.collection("users").doc(req.user.uid)
+            const userdata = (await userRef.get()).data()
+
+           
+            const verification = {
+                ...userdata.verification,
+                phoneNumber: "completed"
+            }
+            await userRef.update({verification})
+            res.status(200).json({mgs: "accepted"})
+        
+        
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({msg: "server erroe"})
+    }
+    }).catch(e=>{
+        console.log(e)
+        res.status(500).json({msg: "server error"})
+    })
+
+    
+
+
+    
+})
+
+
+app.listen(port,()=>{
+    console.log(`listening on port ${port}`);
+})
---
+++ src/utils.js
@@ -0,0 +1,51 @@
+const nodemailer = require('nodemailer');
+
+const generateFourDigitCode = () => {
+    const min = 1000;
+    const max = 9999;
+    const randomCode = Math.floor(Math.random() * (max - min + 1)) + min;
+
+    return randomCode;
+}
+
+
+
+  // Create a Nodemailer transporter using OAuth2
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: "deybollar@gmail.com",
+      pass:"ngya vngb klgd rwqm"
+    }
+  });
+
+  
+
+
+const sendVerificationEmail = async(toEmail, verificationCode,callback) => {
+    const mailOptions = {
+      from: "deybollar@gmail.com",
+      to: toEmail,
+      subject: 'Email Verification Code',
+      text: `Your verification code is: ${verificationCode}`
+    };
+  
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error(error);
+        callback(error)
+      } else {
+        console.log('Email sent: ' + info.response);
+        callback(error)
+      }
+    });
+  };
+
+
+
+
+
+  //twillio verification
+
+
+  module.exports = {sendVerificationEmail, generateFourDigitCode}
